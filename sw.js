da569d06ef3a16dac9ac9ee747d38b533cfb2f63
loterpraca <loterpraca@gmail.com>
2026-01-31 12:32:13 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bolo-pwa-v140"; // suba a versão sempre que mexer em assets
+const CACHE_NAME = "bolo-pwa-v141"; // suba a versão sempre que mexer em assets
 
 const ASSETS = [
   "/bolo-pwa/",
